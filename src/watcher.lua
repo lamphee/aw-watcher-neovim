5d0eb87ad67e93e960284e3c203242985f36b153
lamphee <155618212+lamphee@users.noreply.github.com>
2024-11-24 05:19:19 -0400

    Refactor Remove Unsuded debug commands

--- src/watcher.lua
+++ src/watcher.lua
@@ -1,110 +1,86 @@
-api_url = '127.0.0.1'
-api_port = '5600'
+API_URL = "127.0.0.1"
+API_PORT = "5600"
 
-hostname = vim.fn.hostname()
-event_type = 'app.editor.activity'
-bucket_name = 'aw-watcher-nvim'
-bucket_id = string.format('%s_%s', bucket_name, hostname)
+HOSTNAME = vim.fn.hostname()
+EVENT_TYPE = "app.editor.activity"
+BUCKET_NAME = "aw-watcher-nvim"
+BUCKET_ID = string.format("%s_%s", BUCKET_NAME, HOSTNAME)
 
-api_base = string.format('http://%s:%s/api/0', api_url, api_port)
-api_bucket = string.format('%s/buckets/%s', api_base, bucket_id)
-api_heartbeat = string.format('%s/heartbeat?pulsetime=0', api_bucket)
+API_BASE = string.format("http://%s:%s/api/0", API_URL, API_PORT)
+API_BUCKET = string.format("%s/buckets/%s", API_BASE, BUCKET_ID)
+API_HEARTBEAT = string.format("%s/heartbeat?pulsetime=0", API_BUCKET)
 
-group_id = nil
+AUTOCMD_GROUP_NAME = "activity-watcher-neovim"
+
+GROUP_ID = vim.api.nvim_create_augroup(AUTOCMD_GROUP_NAME, { clear = true })
 
 local function post_data(url, data)
 	print("posting data")
 	local post = {
-		'curl',
-		'-X', 'POST',
-		'--header', 'Content-Type: application/json',
-		'--header', 'Accept: application/json',
-		'--data', vim.fn.json_encode(data),
-		url
+		"curl",
+		"-X",
+		"POST",
+		"--header",
+		"Content-Type: application/json",
+		"--header",
+		"Accept: application/json",
+		"--data",
+		vim.fn.json_encode(data),
+		url,
 	}
 
 	return vim.system(post)
 end
 
-function start()
+function Start()
 	local function create_bucket()
-		res = post_data(api_bucket,
-		{client = bucket_name, hostname = hostname, type = event_type})
+		return post_data(API_BUCKET, { client = BUCKET_NAME, hostname = HOSTNAME, type = EVENT_TYPE })
 	end
 
-	local function create_heartbeat_event(group_id)
-		return vim.api.nvim_create_autocmd(
-		{'BufNewFile', 'CursorMoved',
-		'CursorMovedI', 'ModeChanged',
-		'FocusGained', 'BufEnter',
-		'CmdlineEnter', 'CmdlineChanged'}, { callback = heartbeat,
-		group = group_id })
+	local function create_autogroup()
+		GROUP_ID = vim.api.nvim_create_augroup("activity-watcher-neovim", { clear = true })
 	end
 
-	local function create_augroup()
-		return vim.api.nvim_create_augroup('activity-watcher-neovim', {clear = false})
+	local function create_heartbeat_event(GROUP_ID)
+		return vim.api.nvim_create_autocmd({
+			"BufNewFile",
+			"CursorMoved",
+			"CursorMovedI",
+			"ModeChanged",
+			"FocusGained",
+			"BufEnter",
+			"CmdlineEnter",
+			"CmdlineChanged",
+		}, { callback = Heartbeat, group = GROUP_ID })
 	end
 
 	create_bucket()
-	group_id = create_augroup()
+	create_autogroup()
 
-	create_heartbeat_event(group_id)
+	create_heartbeat_event(GROUP_ID)
 end
 
-function stop()
-	if group_id == nil then
-		return false
-	end
-	return vim.api.nvim_del_augroup_by_id(group_id)
-end
-
--- Add debug prints (vim.fn.debug)
-function heartbeat()
-	heartbeat_data = {
-		timestamp = vim.fn.strftime('%FT%H:%M:%S%z'),
+function Heartbeat()
+	local heartbeat_data = {
+		timestamp = vim.fn.strftime("%FT%H:%M:%S%z"),
 		duration = 0,
 		data = {
-			file = vim.fn.expand('%:p'),
+			file = vim.fn.expand("%:p"),
 			project = vim.fn.getcwd(),
-			language = vim.fn.expand('%:e')
-		}
+			language = vim.fn.expand("%:e"),
+		},
 	}
 
-	return post_data(api_heartbeat, heartbeat_data)
+	return post_data(API_HEARTBEAT, heartbeat_data)
 end
 
-function debug_func(func_name)
-	local function log(func)
-		print(string.format("Executing func <%s>", vim.fn.string(func)))
-		func()
-		print(string.format("Execution of func <%s> ended", vim.fn.string({func})))
+function Stop()
+	if GROUP_ID == nil then
+		return false
 	end
-
-	-- ??
-	-- print(vim.fn.funcref(func_name))
-	funcs = {
-		AWStart = start,
-		AWStop = stop,
-		AWHeartbeat = heartbeat
-	}
-
-
-	ref = funcs[func_name]
-	if ref == nil then
-		print(string.format("Ref to <%s> is null", func_name))
-		return
-	end
-
-	log(ref)
+	return vim.api.nvim_del_augroup_by_id(GROUP_ID)
 end
 
--- map user command to func that declares and starts autocommand
--- (autogroups for managing multiple references)
-vim.api.nvim_create_user_command('AWStart', start, {})
-vim.api.nvim_create_user_command('AWStop', stop, {})
-vim.api.nvim_create_user_command('AWHeartbeat', heartbeat, {})
-
-vim.api.nvim_create_user_command('AWDebug',
-function(args) debug_func(args.args) end,
-{nargs = 1})
-
+vim.api.nvim_create_user_command("AWStart", Start, {})
+vim.api.nvim_create_user_command("AWStop", Stop, {})
+vim.api.nvim_create_user_command("AWHeartbeat", Heartbeat, {})
